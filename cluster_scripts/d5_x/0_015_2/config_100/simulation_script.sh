8b1880025b0577274cf402e2fc1f91f2266c0c41
liangqiyao990210 <liangqiyao99@gmail.com>
2022-12-13 14:41:25 -0500

    config

--- cluster_scripts/d5_x/0_015_2/config_100/simulation_script.sh
+++ cluster_scripts/d5_x/0_015_2/config_100/simulation_script.sh
@@ -2,10 +2,10 @@
 #SBATCH -p scavenger                         # scavenger division
 #SBATCH -c 1                                 # Number of cores
 #SBATCH --array=1-1  			             # How many jobs do you have                               
-#SBATCH --job-name=0.015_12          # Job name, will show up in squeue output
+#SBATCH --job-name=0.015_100          # Job name, will show up in squeue output
 #SBATCH --mail-type=END
 #SBATCH --mail-user=ql94@duke.edu	       # It will send you an email when the job is finished. 
-#SBATCH --mem=15G                   # Memory per cpu in MB (see also --mem) 
+#SBATCH --mem=32G                   # Memory per cpu in MB (see also --mem) 
 #SBATCH --output=out.out         # File to which standard out will be written
 #SBATCH --error=slurm.err           # File to which standard err will be written
 
@@ -23,7 +23,7 @@ scontrol show job $SLURM_JOBID
 
 # ------- run the script -----------------------
 
-python /hpc/home/ql94/DeepQ-Decoding/cluster_scripts/d5_x/0_015/Single_Point_Continue_Training_Script.py 12
+python /hpc/home/ql94/DeepQ-Decoding/cluster_scripts/d5_x/0_015_2/Single_Point_Continue_Training_Script.py 100
 
 #----------- wait some time ------------------------------------
 
